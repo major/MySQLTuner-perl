73d0dd9d75d2080e7d55422033fbdb26ba07054a
root <jmrenouard@gmail.com>
2015-06-18 23:26:02 +0200

    Full feature build system for mysqltuner.

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-VERSION=1.4.5
+BUILD_DIR=`dirname $(readlink -f $0)`
 
-mkdir -p mysqltuner-$VERSION
-cp ../mysqltuner.pl mysqltuner.spec ../LICENSE mysqltuner-$VERSION
-tar czf mysqltuner-${VERSION}.tgz mysqltuner-$VERSION
+VERSION=$(grep -i tunerversion $BUILD_DIR/../mysqltuner.pl | grep = | cut -d\" -f2)
+cd $BUILD_DIR
+perl -pe "s/%VERSION%/$VERSION/g" mysqltuner.spec.tpl > mysqltuner.spec
+mkdir -p $BUILD_DIR/mysqltuner-$VERSION
+cp $BUILD_DIR/../mysqltuner.pl mysqltuner.spec $BUILD_DIR/../LICENSE $BUILD_DIR/mysqltuner-$VERSION
+tar czf $BUILD_DIR/mysqltuner-${VERSION}.tgz mysqltuner-$VERSION
 rpmbuild -ta mysqltuner-${VERSION}.tgz 2>/dev/null| grep --color=never '\.rpm' | cut -d: -f2 > ./lrpm.txt
 mv $(cat ./lrpm.txt) .
 rm -rf mysqltuner-$VERSION ./lrpm.txt
