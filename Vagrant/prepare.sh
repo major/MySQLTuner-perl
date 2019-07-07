42a9586aaf615f533bf05d0365ccd93c35045468
Jean-Marie Renouard <jmrenouard@gmail.com>
2019-07-07 09:58:02 +0200

    Refresh config. Vagrant
    Update vagrantfile.

--- Vagrant/prepare.sh
+++ Vagrant/prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+VERSION=${1:-"10.4"}
 rm -f Vagrantfile
-cp Vagrantfile_for_MariaDB10.0 Vagrantfile
+cp Vagrantfile_for_MariaDB${VERSION} Vagrantfile
 mkdir data
 vagrant plugin install vagrant-hostmanager
 vagrant plugin install vagrant-vbguest
