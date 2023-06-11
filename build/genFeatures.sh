e86778b43bdf606e700df3740169810c8da26d00
Jean-Marie Renouard <jmrenouard@gmail.com>
2023-06-11 12:09:10 -0400

    Laste version with clean FEATURES.md list

--- build/genFeatures.sh
+++ build/genFeatures.sh
@@ -2,11 +2,12 @@
 
 # Update Feature list
 (
+    export LANG=C
     echo -e "Features list for option: --feature (dev only)\n---\n\n"
     grep -E '^sub ' ../mysqltuner.pl | \
-    perl -pe 's/sub //;s/\s*\{//g'| \
+    perl -pe 's/sub //;s/\s*\{//g' | \
     sort -n | \
     perl -pe 's/^/* /g' | \
-    grep -vE '(is_|hr_|info)'
+    grep -vE '(get_|close_|check_|memerror|human_size|string2file|file2|arr2|dump|which|percentage|trim|is_|hr_|info|print|select|wrap|remove_)'
 ) > ../FEATURES.md
-
+cat ../FEATURES.md
