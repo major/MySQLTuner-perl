e1da1aad951b242ca9ad79135c07e8ccc350cdaf
Jean-Marie Renouard <jmrenouard@gmail.com>
2023-06-17 22:37:09 +0200

    update version number

--- mysqltuner.pl
+++ mysqltuner.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# mysqltuner.pl - Version 2.1.5
+# mysqltuner.pl - Version 2.1.6
 # High Performance MySQL Tuning Script
 # Copyright (C) 2006-2023 Major Hayden - major@mhtx.net
 # Copyright (C) 2015-2023 Jean-Marie Renouard - jmrenouard@gmail.com
@@ -57,7 +57,7 @@ use Cwd 'abs_path';
 #use Env;
 
 # Set up a few variables for use in the script
-my $tunerversion = "2.1.5";
+my $tunerversion = "2.1.6";
 my ( @adjvars, @generalrec );
 
 # Set defaults
@@ -7059,7 +7059,7 @@ __END__
 
 =head1 NAME
 
- MySQLTuner 2.1.5 - MySQL High Performance Tuning Script
+ MySQLTuner 2.1.6 - MySQL High Performance Tuning Script
 
 =head1 IMPORTANT USAGE GUIDELINES
 
