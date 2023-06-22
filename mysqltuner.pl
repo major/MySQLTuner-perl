6cd1c4197d28769f7b0c259a31b0af4944fb6efe
Jean-Marie Renouard <jmrenouard@gmail.com>
2023-06-22 10:10:56 +0200

    Not working on MariaDB 10.6.10 on Ubuntu 18 #620 MT is handling innodb_log_files_in_group = 0 setting

--- mysqltuner.pl
+++ mysqltuner.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# mysqltuner.pl - Version 2.1.8
+# mysqltuner.pl - Version 2.1.9
 # High Performance MySQL Tuning Script
 # Copyright (C) 2006-2023 Major Hayden - major@mhtx.net
 # Copyright (C) 2015-2023 Jean-Marie Renouard - jmrenouard@gmail.com
@@ -57,7 +57,7 @@ use Cwd 'abs_path';
 #use Env;
 
 # Set up a few variables for use in the script
-my $tunerversion = "2.1.8";
+my $tunerversion = "2.1.9";
 my ( @adjvars, @generalrec );
 
 # Set defaults
@@ -3060,6 +3060,8 @@ sub calculations {
     # InnoDB
     $myvar{'innodb_log_files_in_group'} = 1
       unless defined( $myvar{'innodb_log_files_in_group'} );
+    $myvar{'innodb_log_files_in_group'} = 1 if $myvar{'innodb_log_files_in_group'} == 0;
+
     $myvar{"innodb_buffer_pool_instances"} = 1
       unless defined( $myvar{'innodb_buffer_pool_instances'} );
     if ( $myvar{'have_innodb'} eq "YES" ) {
@@ -7097,7 +7099,7 @@ __END__
 
 =head1 NAME
 
- MySQLTuner 2.1.8 - MySQL High Performance Tuning Script
+ MySQLTuner 2.1.9 - MySQL High Performance Tuning Script
 
 =head1 IMPORTANT USAGE GUIDELINES
 
