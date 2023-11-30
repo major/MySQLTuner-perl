e8ecc98585b37963e8f1f21fadd175b277ae8673
Jean-Marie Renouard <jmrenouard@gmail.com>
2023-11-30 11:35:03 +0100

    Generate 2.4.0 minor version at 2023-11-30T11:35:00+01:00

--- mysqltuner.pl
+++ mysqltuner.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# mysqltuner.pl - Version 2.3.2
+# mysqltuner.pl - Version 2.4.0
 # High Performance MySQL Tuning Script
 # Copyright (C) 2015-2023 Jean-Marie Renouard - jmrenouard@gmail.com
 # Copyright (C) 2006-2023 Major Hayden - major@mhtx.net
@@ -57,7 +57,7 @@ use Cwd 'abs_path';
 #use Env;
 
 # Set up a few variables for use in the script
-my $tunerversion = "2.3.2";
+my $tunerversion = "2.4.0";
 my ( @adjvars, @generalrec );
 
 # Set defaults
@@ -2526,7 +2526,7 @@ sub check_architecture {
     }
     elsif ( `uname` =~ /Darwin/ && `uname -m` =~ /x86_64/ ) {
 
-# Darwin gibas.local 12.3.2 Darwin Kernel Version 12.3.0: Sun Jan 6 22:37:10 PST 2013; root:xnu-2050.22.13~1/RELEASE_X86_64 x86_64
+# Darwin gibas.local 12.4.0 Darwin Kernel Version 12.3.0: Sun Jan 6 22:37:10 PST 2013; root:xnu-2050.22.13~1/RELEASE_X86_64 x86_64
         $arch = 64;
         goodprint "Operating on 64-bit architecture";
     }
@@ -3179,11 +3179,11 @@ sub calculations {
       = ( 1, 1 )
       unless defined $mystat{'Innodb_buffer_pool_reads'};
     $mycalc{'pct_read_efficiency'} = percentage(
-        (
-            $mystat{'Innodb_buffer_pool_read_requests'} -
+        $mystat{'Innodb_buffer_pool_read_requests'},
+				(
+            $mystat{'Innodb_buffer_pool_read_requests'} +
               $mystat{'Innodb_buffer_pool_reads'}
-        ),
-        $mystat{'Innodb_buffer_pool_read_requests'}
+        )
     ) if defined $mystat{'Innodb_buffer_pool_read_requests'};
     debugprint "pct_read_efficiency: " . $mycalc{'pct_read_efficiency'} . "";
     debugprint "Innodb_buffer_pool_reads: "
@@ -6513,19 +6513,17 @@ sub mysql_innodb {
     {
         badprint "InnoDB Read buffer efficiency: "
           . $mycalc{'pct_read_efficiency'} . "% ("
-          . ( $mystat{'Innodb_buffer_pool_read_requests'} -
-              $mystat{'Innodb_buffer_pool_reads'} )
-          . " hits / "
           . $mystat{'Innodb_buffer_pool_read_requests'}
+          . " hits / "
+          . ( $mystat{'Innodb_buffer_pool_reads'} + $mystat{'Innodb_buffer_pool_read_requests'} )
           . " total)";
     }
     else {
         goodprint "InnoDB Read buffer efficiency: "
           . $mycalc{'pct_read_efficiency'} . "% ("
-          . ( $mystat{'Innodb_buffer_pool_read_requests'} -
-              $mystat{'Innodb_buffer_pool_reads'} )
-          . " hits / "
           . $mystat{'Innodb_buffer_pool_read_requests'}
+          . " hits / "
+          . ( $mystat{'Innodb_buffer_pool_reads'} + $mystat{'Innodb_buffer_pool_read_requests'} )
           . " total)";
     }
 
@@ -7369,7 +7367,7 @@ __END__
 
 =head1 NAME
 
- MySQLTuner 2.3.2 - MySQL High Performance Tuning Script
+ MySQLTuner 2.4.0 - MySQL High Performance Tuning Script
 
 =head1 IMPORTANT USAGE GUIDELINES
 
