17b9e6cb31100bd78e4e2991d9a71c38f440ae99
Jean-Marie Renouard <jmrenouard@gmail.com>
2026-01-18 00:37:01 +0100

    chore: bump version to 2.8.16

--- mysqltuner.pl
+++ mysqltuner.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# mysqltuner.pl - Version 2.8.15
+# mysqltuner.pl - Version 2.8.16
 # High Performance MySQL Tuning Script
 # Copyright (C) 2015-2023 Jean-Marie Renouard - jmrenouard@gmail.com
 # Copyright (C) 2006-2023 Major Hayden - major@mhtx.net
@@ -59,7 +59,7 @@ use Cwd 'abs_path';
 my $is_win = $^O eq 'MSWin32';
 
 # Set up a few variables for use in the script
-my $tunerversion = "2.8.15";
+my $tunerversion = "2.8.16";
 my ( @adjvars, @generalrec );
 
 # Set defaults
@@ -8292,7 +8292,7 @@ You must provide the remote server's total memory when connecting to other serve
 
 =head1 VERSION
 
-Version 2.8.15
+Version 2.8.16
 =head1 PERLDOC
 
 You can find documentation for this module with the perldoc command.
